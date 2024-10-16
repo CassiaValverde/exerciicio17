00ce473b1b20621fd80871d2f65d3710664bdbdb
CassiaValverde <126363351+CassiaValverde@users.noreply.github.com>
2024-10-16 17:36:26 -0300

    Add files via upload

---
+++ listaDeExercicio17.py
@@ -0,0 +1,5 @@
+#calcular velocidade média
+distancia = float(input("Qual a distância? \n"))
+tempo = float(input("Informe a velocidade \n"))
+velocidade_media = distancia/tempo
+print(f"A velocidade média é {velocidade_media:.2f}")
